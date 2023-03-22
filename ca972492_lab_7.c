9a7f1c50e7183fc6cc36021da88b3ce89af093e9
carelos <125504570+carelos@users.noreply.github.com>
2023-03-22 15:33:41 -0400

    Add files via upload

---
+++ ca972492_lab_7.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	
+	for (i = 0; i < size; i++)  
+		printf("%d ", arr[i]);
+		
+	printf("\n\n");
+}
+
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	int count = 0;
+	int iteration = 1;
+	
+	for (i = 0; i < (n-1); i++)
+	{
+		for (j = 0; j < (n-i-1); j++)  
+		{
+			if (arr[j] > arr[j+1])
+			{
+				swap(&arr[j], &arr[j+1]);
+				count++;
+			}
+		}
+		
+		printf("Iteration #%d: %d swaps\n", iteration, count);
+
+		iteration++;
+		count = 0;
+	}
+	
+	printf("\n");
+}
+ 
+int main()
+{
+	int arr[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+	printf("\nArray before bubble sort: \n");
+	
+	int n = 9;  
+	printArray(arr, n);
+	
+	bubbleSort(arr, n);
+	
+	printf("Sorted array: \n");  
+	printArray(arr, n);
+	
+	return 0;
+}
